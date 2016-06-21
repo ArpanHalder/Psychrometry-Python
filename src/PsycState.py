0f15c498c0bb5d18973683a3dcb90d7cc1ceed2d
Arpan Halder <tech.halder.arpan@gmail.com>
2016-06-21 22:35:46 +0530

    initial

---
+++ src/PsycState.py
@@ -0,0 +1,10 @@
+class PsycState :
+    
+    def __init__(self, Td = 27, Tw = 25) :
+        
+        self.Td = Td;
+        self.Tw = Tw;
+        self.B = 101.325;
+        
+    @classmethod
+    def 
